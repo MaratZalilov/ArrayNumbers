282dba3afdac87970a10b23c083469e69236981a
Марат <@DESKTOP-LLV2BAH>
2022-12-04 15:14:37 +0300

    Добавьте файлы проекта.

---
+++ ArrayNumbers/Number.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayNumbers
+{
+    public class Number
+    {
+
+        public void OddNumber(int[] arrayNumbers)
+        {
+            List<int> numbers = new List<int>();
+            for(int i = 0; i < arrayNumbers.Length; i++)
+            {
+                int count = 0;
+                var numbersInArray = arrayNumbers[i];
+                foreach(var check in arrayNumbers)
+                {
+                    if(check == numbersInArray)
+                    {
+                        count++;
+                    }
+                }
+                if (count % 2 != 0 && !numbers.Contains(numbersInArray))
+                {
+                    numbers.Add(numbersInArray);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Эти числа встречаются нечётное кол-во раз - ");
+            foreach(var check in numbers)
+            {
+                Console.Write($"{check}");
+            }
+            Console.WriteLine();
+        }
+
+        public void EvenNumber(int[] arrayNumbers)
+        {
+            List<int> numbers = new List<int>();
+            for(int i = 0; i < arrayNumbers.Length; i++)
+            {
+                int count = 0;
+                var numbersInArray = arrayNumbers[i];
+                foreach(var check in arrayNumbers)
+                {
+                    count++;
+                }
+                if (count % 2 == 0 && !numbers.Contains(numbersInArray))
+                {
+                    numbers.Add(numbersInArray);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Эти числа встречаются чётное кол-во раз - ");
+            foreach(var check in numbers)
+            {
+                Console.Write($"{check}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ArrayNumbers/Program.cs
@@ -0,0 +1,6 @@
+﻿using ArrayNumbers;
+
+Number number = new Number();
+int[] arrayNumbers = new int[] { 1, 2, 3, 4, 4, 6, 7, 7, 7, 10, 1, 6, 6, 6, 11, 11, };
+number.OddNumber(arrayNumbers);
+number.EvenNumber(arrayNumbers);
